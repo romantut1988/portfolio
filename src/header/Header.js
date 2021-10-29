7f26aefa420507f0c61450a79f30d50adbaabcd9
Roman <romantut1988@mail.ru>
2021-10-30 01:15:07 +0300

    created structure and nav file

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import style from './Header.module.css';
+import Nav from "../nav/Nav";
 
 
 function Header() {
     return (
         <div className={style.header}>
-
+            <Nav />
         </div>
     );
 }
