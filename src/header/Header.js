055d92ba0c9a91f9c6b0b76a616d1692188ed330
Roman <romantut1988@mail.ru>
2021-10-30 00:21:22 +0300

    created structure and file header

---
+++ src/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './Header.module.css';
+
+
+function Header() {
+    return (
+        <div className={style.header}>
+
+        </div>
+    );
+}
+
+export default Header;
