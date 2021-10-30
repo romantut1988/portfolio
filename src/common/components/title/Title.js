732be8c8d2bbb7cec8872cbc533dd1472ea58a4d
Roman <romantut1988@mail.ru>
2021-10-31 00:42:50 +0300

    added fonts to the project

---
+++ src/common/components/title/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import style from './Title.module.css';
+
+
+function Title() {
+    return (
+
+        <div className={style.title}>
+            <h2>Projects</h2>
+        </div>
+
+    );
+}
+
+export default Title;
--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import styleContainer from './../common/styles/Container.module.css';
+import styleContainer from '../common/styles/Container.module.css';
 import style from './Projects.module.css';
-import Skill from "./projects/Projects";
+import Project from './project/Project';
+import Title from '../common/components/title/Title';
+
+
+
 
 function Projects() {
     return (
         <div className={style.projectsBlock}>
             <div className={`${styleContainer.container} ${style.projectsContainer}`}>
-                <h2 className={style.title}>Projects</h2>
+                <Title />
                 <div className={style.projects}>
-                    <Projects title={'Social network'} desccription={'sed do eismod tempor incudidunt ut labore et delore magna aliqua Ut enim...'}/>
-                    <Projects title={'Todo list'} desccription={'Lorem ipsum dolor sit amet, consectetur adipisicing elit,'}/>
+                    <Project title={'Social network'} description={'sed do eismod tempor incudidunt ut labore et delore magna aliqua Ut enim...'}/>
+                    <Project title={'Todo list'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit,'}/>
                 </div>
             </div>
         </div>
