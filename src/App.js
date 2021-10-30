b93a4ae1b58c6c53e8b131cd3ffd34bf773e53f1
Roman <romantut1988@mail.ru>
2021-10-30 10:30:38 +0300

    created a new folder for container styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from "./header/Header";
-
+import Main from "./main/Main";
+import Nav from "./nav/Nav";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
+      <Nav />
     </div>
   );
 }
---
+++ src/main/Main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import style from './Main.module.css';
+import styleContainer from './../common/style/Container.module.css';
+
+
+function Main() {
+    return (
+        <div className={style.mainBlock}>
+            <div className={styleContainer.container}>
+                <span>Hi There</span>
+                <h1>I am Jefferay Aaron</h1>
+                <p>Frontend Developer.</p>
+                <div className={style.photo}></div>
+            </div>
+        </div>
+
+    );
+}
+
+export default Main;
