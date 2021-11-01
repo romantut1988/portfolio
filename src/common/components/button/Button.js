641ef881bd2246ac4c6e8db5efc3933bbb47de26
Roman <romantut1988@mail.ru>
2021-11-01 23:37:24 +0300

    styles for the button

---
+++ src/common/components/button/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './Button.module.scss';
+
+function Button(props) {
+    return (
+        <a href='' className={style.btn}>{props.text}</a>
+    );
+}
+
+export default Button;
