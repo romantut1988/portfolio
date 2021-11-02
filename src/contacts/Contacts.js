7b2985b8f91e5bef2005a861969041fb5aeecef1
Roman <romantut1988@mail.ru>
2021-11-03 00:28:33 +0300

    profile photo

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import style from './Contacts.module.css';
-import styleContainer from '../common/styles/Container.module.css';
-
-
-
-
+import Title from '../common/components/title/Title';
+import Fade from 'react-reveal/Fade';
 
 function Contacts() {
     return (
         <div className={style.contactsBlock}>
             <div className={style.container}>
                 <Title text={'Contacts'} />
+                <Fade left>
                 <form className={style.form}>
                     <input type='text' className={style.formArea} placeholder='Name'/>
                     <input type='text' className={style.formArea} placeholder='e-mail'/>
                     <textarea className={style.messageArea} placeholder='Message'/>
                     <button type='submit'>Send message</button>
                 </form>
+                </Fade>
             </div>
         </div>
     );
--- src/main/Main.js
+++ src/main/Main.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import style from './Main.module.css';
+import style from './Main.module.scss';
 import Particles from 'react-particles-js';
+import Fade from 'react-reveal/Fade';
+import ReactTypingEffect from 'react-typing-effect';
+
 
 
 const particlesOptions = {
@@ -19,19 +22,23 @@ function Main() {
     return (
         <div className={style.mainBlock}>
             <Particles className={style.particles} params={particlesOptions} />
+            <Fade top>
             <div className={style.container}>
                 <div className={style.greeting}>
                 <span>Hi There</span>
                     <span>I am Roman<span>Amialkovich</span></span>
                 <h1>Web.</h1>
+                <ReactTypingEffect
+                    text='Web'.
+                />
                 </div>
                 <div className={style.photo}>
                     <div className={style.image}>
 
                     </div>
                 </div>
-
             </div>
+            </Fade>
         </div>
     );
 }
