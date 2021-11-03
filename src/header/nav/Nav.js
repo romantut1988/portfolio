927b5a093a64db9f20bba5f751493e490e87cfdb
Roman <romantut1988@mail.ru>
2021-11-03 03:01:24 +0300

    portfolio test

--- src/header/nav/Nav.js
+++ src/header/nav/Nav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import style from './Nav.module.css';
-
+import { Link, animateScroll as scroll} from 'react-scroll';
 
 function Nav() {
     return (
@@ -8,6 +8,13 @@ function Nav() {
             <a href="">Main</a>
             <a href="">Skills</a>
             <a href='#projects'>Projects</a>
+                <Link
+                    activeClass={style.active}
+                    to='projects'
+                    smooth={true}
+                    ofset={1}
+                    duration={500}
+                    >Project</Link>
             <a href="">Contacts</a>
         </div>
     );
